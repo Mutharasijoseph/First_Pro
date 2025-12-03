c65a3e72ed8ffa20d62e89e0cb34efaf27c3ea84
mutharasi <mutharasijoseph@gmail.com>
2025-12-03 21:43:34 +0530

    My work update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import NewComponent from './NewComponent';
 
 export default function App() {
+  const [showNew, setShowNew] = useState(false);
+
   return (
     <div className="app">
-      <h1>Welcome to First_Project</h1>
-      <button>Get Started</button>
+      {showNew ? (
+        <NewComponent />
+      ) : (
+        <>
+          <h1>Welcome to First_Project</h1>
+          <button onClick={() => setShowNew(true)}>Get Started</button>
+        </>
+      )}
     </div>
   );
 }
